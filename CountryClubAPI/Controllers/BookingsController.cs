43596147f44eb2359b2be29c5b151036604a45a5
joe10111 <centeno.joe@gmail.com>
2023-10-09 13:17:56 -0700

    Booking Creation Done

---
+++ CountryClubAPI/Controllers/BookingsController.cs
@@ -0,0 +1,43 @@
+﻿using CountryClubAPI.DataAccess;
+using CountryClubAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CountryClubAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+
+        private readonly CountryClubContext _context;
+
+        public BookingsController(CountryClubContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult AllBookings()
+        {
+            var bookings = _context.Bookings;
+
+            return new JsonResult(bookings);
+        }
+
+        [HttpPost]
+        public ActionResult CreateBooking(Booking booking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Bookings.Add(booking);
+            _context.SaveChanges();
+
+            Response.StatusCode = 200;
+
+            return new JsonResult(booking);
+        }
+    }
+}
